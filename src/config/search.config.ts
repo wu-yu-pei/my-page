6bd5dce511027fc0140d689996dcd2fbfc591126
wuyupei <495174699@qq.com>
2022-05-09 20:51:58 +0800

    fix searchOrigin

--- src/config/search.config.ts
+++ src/config/search.config.ts
@@ -1,8 +1,8 @@
 export const searchConfig: any = [
-  { name: 'Google', url: 'https://www.google.com/search?q=' },
   { name: 'Bing', url: 'https://www.bing.com/search?q=' },
   { name: '搜狗', url: 'https://sogou.com/web?q=' },
+  { name: 'Bilibili', url: 'https://search.bilibili.com?keyword=' },
+  { name: 'Google', url: 'https://www.google.com/search?q=' },
   { name: 'npm', url: 'https://www.npmjs.com/search?q=' },
   { name: 'MDN', url: 'https://developer.mozilla.org/zh-CN/search?q=' },
-  { name: 'Bilibili', url: 'https://search.bilibili.com?keyword=' },
 ];
