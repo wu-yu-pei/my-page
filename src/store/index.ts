f2ad2be2ae51d3b73cba9657dffd29d89b5a6e11
wuyupei <495174699@qq.com>
2022-05-13 16:18:51 +0800

    fix default.jpg

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,7 +5,7 @@ const useMainStore = defineStore('Main', {
   // arrow function recommended for full type inference
   state: () => {
     return {
-      WallpaperImgUlr: localStorage.getItem('bg-image') || 'http://wuyupei.top:8888/upload/d7a2f64f6594e46e198dab001.jpg',
+      WallpaperImgUlr: localStorage.getItem('bg-image') || 'http://wuyupei.top:8888/upload/default.jpg',
       blur: Number(localStorage.getItem('blur')) || 0,
       radius: Number(localStorage.getItem('radius')) || 0,
       muenSource:
