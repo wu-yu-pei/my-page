be4452524dcb82ee89c4ddbdbcc0bfda8416de3a
Wuyupei <495174699@qq.com>
2022-07-30 19:21:12 +0800

    stop

--- src/base-ui/DockItem.vue
+++ src/base-ui/DockItem.vue
@@ -5,7 +5,7 @@
     ref="the"
     :target="target"
     @mousedown="showDelete"
-    @contextmenu="(e) => e.preventDefault()"
+    @contextmenu.stop="(e) => e.preventDefault()"
   >
     <img :src="source.icon" alt="" :style="{ borderRadius: `${store.radius}px` }" />
     <div class="dock-item-info">{{ source.des }}</div>
