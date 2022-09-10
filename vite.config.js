0b657a4aa2f51d9dd481e5d14719ae0b10fc43a1
Wuyupei <495174699@qq.com>
2022-09-10 09:48:50 +0800

    gzip

--- vite.config.js
+++ vite.config.js
@@ -30,7 +30,7 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if ((id as any).includes('node_modules')) {
+          if (id.includes('node_modules')) {
             return id.toString().split('node_modules/.pnpm/')[1].split('/')[0].toString();
           }
         },
