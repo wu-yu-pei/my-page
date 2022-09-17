23aa625fe62b9b272cda83c4b337f6c446b9531b
Wuyupei <495174699@qq.com>
2022-09-17 22:15:09 +0800

    优化

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
-  <div
-    class="content"
-    :style="{ background: `url(${state.WallpaperImgUlr})`, backgroundSize: 'cover' }"
-  >
-    <div class="filter" :style="{ backdropFilter: `blur(${state.blur}px)` }">
+  <div class="content" ref="contentEl">
+    <div class="filter" ref="filterEl">
       <Header />
       <Body />
     </div>
@@ -11,6 +8,8 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
 import Header from './components/Header.vue';
 import Body from './components/Body.vue';
 
@@ -18,22 +17,34 @@ import useMainStore from './store/index';
 import useSyncConfig from './hooks/useSyncConfig';
 
 const state = useMainStore();
+const contentEl = ref(null);
+const filterEl = ref(null);
 
 if (state.userId) {
   useSyncConfig(state.userId);
 }
+onMounted(() => {
+  const image = new Image();
+  image.src = state.WallpaperImgUlr;
+  image.onload = () => {
+    (contentEl.value as any).style.background = `url(${state.WallpaperImgUlr})`;
+    (filterEl.value as any).style.backdropFilter = `blur(${state.blur}px)`;
+  };
+});
 </script>
 
-<style>
+<style scoped>
 .content {
   width: 100%;
   height: 100%;
   margin: 0 auto;
   overflow: hidden;
+  background-color: skyblue;
   background-size: cover;
 }
 .filter {
   width: 100vw;
   height: 100vh;
+  backdrop-filter: 100px;
 }
 </style>
